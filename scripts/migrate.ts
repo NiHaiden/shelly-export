26c161e9074160a5f9998b0c122f7ff18adda7c4
Niklas Haiden <niklas.haiden@gmail.com>
2025-12-17 00:23:50 +0100

    chore: do not require ssl for database

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -11,7 +11,7 @@ async function main() {
   const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl:
-      process.env.NODE_ENV === "production"
+      process.env.DATABASE_SSL === "true"
         ? { rejectUnauthorized: false }
         : false,
   });
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,16 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+  ssl:
+    process.env.DATABASE_SSL === "true"
+      ? { rejectUnauthorized: false }
+      : false,
+});
+
+export const db = drizzle(pool, { schema });
